9614b83c71584bab22c821785381cffcacd270fd
jishaocong <574564213@qq.com>
2016-03-17 16:01:00 +0800

    Signed-off-by: jishaocong <574564213@qq.com>

--- src/main/java/com/maiquan/aladdin_mall/controller/UserController.java
+++ src/main/java/com/maiquan/aladdin_mall/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.maiquan.aladdin_mall.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aladdin.account.service.AccountService;
+import com.aladdin.user.entity.BaseCollect;
+import com.aladdin.user.service.UserService;
 import com.maiquan.aladdin_mall.Principal;
 import com.maiquan.aladdin_mall.util.WebUtil;
+import com.maiquan.aladdin_product.service.IProductCollectService;
 
 /**
  * 用户
@@ -20,7 +28,11 @@ import com.maiquan.aladdin_mall.util.WebUtil;
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
+	private UserService userService;
+	@Autowired
 	private AccountService accountService;
+	@Autowired
+	private IProductCollectService productCollectService;
 
 	/**
 	 * 个人中心
@@ -58,5 +70,30 @@ public class UserController {
 	@RequestMapping(value = "/withDraw", method = RequestMethod.GET)
 	public String withDraw() {
 		return "user/withDraw";
+	}
+
+	/**
+	 * 申请提现
+	 * 
+	 * @param money
+	 *            提现金额（分）
+	 * @return
+	 */
+	@RequestMapping(value = "/applyWithDraw", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> applyWithDraw(Integer money) {
+		return accountService.applyWithDraw(money);
+	}
+
+	/**
+	 * 我的收藏
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/collect", method = RequestMethod.GET)
+	public String collect(ModelMap modelMap) {
+		Principal principal = WebUtil.getCurrentPrincipal();
+		modelMap.addAttribute("product", productCollectService.getProductCollectListByIDs("1", null));
+		return "user/collect";
 	}
 }
