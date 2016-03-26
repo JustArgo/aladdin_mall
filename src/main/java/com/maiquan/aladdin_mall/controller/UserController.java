35a7bdfa71cbaaa577ab808dfea3676398101c34
JustArgo <565148812@qq.com>
2016-03-26 09:44:43 +0800

    none

--- src/main/java/com/maiquan/aladdin_mall/controller/UserController.java
+++ src/main/java/com/maiquan/aladdin_mall/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.maiquan.aladdin_mall.controller;
 
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
